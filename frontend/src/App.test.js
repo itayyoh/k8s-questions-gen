9a5776c27df9b9874eed8772725825d30e39bb53
itayyoh <itay.yohanok10@gmail.com>
2025-07-23 22:54:10 +0300

    fix

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders kubernetes interview prep', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/kubernetes interview prep/i);
   expect(linkElement).toBeInTheDocument();
 });
